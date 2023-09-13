0dbbf46ea160c4af34bc74c67ee7883106cb1c06
devin <77528948+zhengdechang@users.noreply.github.com>
2023-09-13 14:12:02 +0800

    Update vercel-server.js

--- app-server/vercel-server.js
+++ app-server/vercel-server.js
@@ -1,8 +1,16 @@
+const http = require('http');
 const egg = require('egg');
 
-const workers = Number(process.argv[2] || require('os').cpus().length);
-egg.startCluster({
-  workers,
-  baseDir: __dirname,
-  port: process.env.PORT || 7001, // Vercel 会提供一个环境变量 PORT
+const server = http.createServer((req, res) => {
+  // 启动 Egg.js
+  const app = egg.start({
+    baseDir: __dirname,
+    mode: 'cluster',
+    port: process.env.PORT || 7001,
+  });
+
+  // 将请求转发给 Egg.js
+  app.callback()(req, res);
 });
+
+server.listen(process.env.PORT || 3000);
