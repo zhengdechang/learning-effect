1b579debbe9eea39f1ec2228634865703f31c798
zhengdechang <git config core.autocrlf false>
2023-09-14 14:09:38 +0800

    update

--- app-server/index.js
+++ app-server/index.js
@@ -1,8 +1,12 @@
+/*
+ * @Description: 
+ * @Author: Devin
+ * @Date: 2023-09-14 13:00:36
+ */
 const egg = require("egg");
 
 egg.startCluster(
   {
-    baseDir: __dirname,
     port: process.env.PORT || 7001, // default to 7001
   },
   () => {
